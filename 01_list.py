901c31abe302b054b66500af471bdab20caa68ca
Soham-Debnath <20debnathsoham04@gmail.com>
2024-12-26 02:23:15 +0530

    List_functions

---
+++ 01_list.py
@@ -0,0 +1,21 @@
+# This code shows List features.
+
+l1=[1,34,62,2,6,11]
+
+# l1.sort()
+# print(l1) #output [1,2,6,11,34,62]
+
+# l1.reverse()
+# print(l1) #output [11,6,2,62,34,1]
+
+# l1.append(4)
+# print(l1) #output [1,34,62,2,6,11,4]
+
+# l1.insert(1,69)
+# print(l1) #output [1,69,34,62,2,6,11]
+
+# l1.pop(2)
+# print(l1) #delete element at index 3 i.e. 62
+
+l1.remove(2)
+print(l1)
